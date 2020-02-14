af16987519ee4407165bc5a8157a83d1ff12ef94
Simo Khouili <m.simokhouili@gmail.com>
2020-02-14 03:19:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,6 @@ def speak_to_grandma(phrase)
   if phrase == 'i love you Grandma'.upcase
     'i love you pumpkin'.upcase
     else
-      'huh? speak up, sonny!'.upcase
+      'i love you pumpkin'.upcase
   end
 end
